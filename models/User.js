4429644592254096b5b5e681df91460032fcccf9
twilight-warlock <devansh.ts@somaiya.edu>
2021-06-08 23:20:25 +0530

    Created user model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+import {Schema, model} from "mongoose"
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String, 
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+export default model("user", UserSchema)
