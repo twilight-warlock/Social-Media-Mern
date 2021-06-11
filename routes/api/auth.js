0c45cd485cd6e70e04959baf4bca3bbaf65b6a52
twilight-warlock <devansh.ts@somaiya.edu>
2021-06-11 22:26:31 +0530

    User authentication route completed

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 import { Router } from "express"
 import auth from "../../middleware/auth.js"
 import User from "../../models/User.js"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import config from "config"
+import { check, validationResult} from "express-validator"
 
 const router = Router()
 
@@ -18,6 +22,60 @@ router.get("/",auth,async (req,res)=>{
         console.error(err.message)
         res.status(500).send("Server Error")
     }
+})
+
+router.post("/",[
+    check("email","Email is not valid").isEmail(),
+    check("password","Password is required").exists()
+    ],
+    async (req,res)=>{
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+
+        const {email, password} = req.body
+
+        try {
+            // Check if user exists
+            let queryResult = await User.findOne({email})
+    
+            // If user does not exist
+            if(!queryResult){
+                return res.status(400).json({errors : [{msg:"Entered invalid credentials"}]})
+            }
+
+            // Using bcrypt to compare inputted password and password from db
+            const passwordCompare = await bcrypt.compare(password,queryResult.password)
+
+            if(!passwordCompare){
+                return res.status(400).json({errors : [{msg:"Entered invalid credentials"}]})
+            }
+
+            // Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: queryResult.id
+                }
+            }
+
+            // Using payload to create encoded token
+            jwt.sign(
+                payload, 
+                config.get("jsonWebTokenSecret"), 
+                {expiresIn: config.get("expiresIn")},
+                (err,token) => {
+                    if(err){
+                        throw err;
+                    }
+                    res.json({token})
+                }
+            )
+            
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server error")
+        }
 })
 
 export default router
