c2fcd70249eb921ac96b09e977a42d42cc429ddf
twilight-warlock <devansh.ts@somaiya.edu>
2021-06-17 00:48:18 +0530

    Added route to add post

--- routes/api/posts/posts.js
+++ routes/api/posts/posts.js
@@ -1,12 +1,52 @@
-import { Router } from "express"
+import { Router } from "express";
+import auth from "../../../middleware/auth.js";
+import joi from "joi";
+import User from "../../../models/User.js";
+import Post from "../../../models/Post.js";
 
-const router = Router()
+const router = Router();
 
 /*
-@route      GET api/posts
-@desc       Get info
-@access     Public
+@route      POST api/posts
+@desc       Create a post
+@access     Private
 */
-router.get("/",(req,res)=>res.send("Posts route"))
+router.post("/", auth, async (req, res) => {
+	const { caption, imageUrl } = req.body;
 
-export default router
+	try {
+		await joi
+			.object({
+				caption: joi.string().required(),
+			})
+			.validateAsync({ caption });
+
+		const user = await User.findById(req.user.id).select("-password");
+
+		const newPost = {
+			caption,
+			name: user.username,
+			avatar: user.avatar,
+			user: req.user.id,
+		};
+
+		if (imageUrl) newPost.imageUrl = imageUrl;
+
+		const post = new Post(newPost);
+		await post.save();
+
+		res.json(post);
+	} catch (error) {
+		console.log(error);
+		if (error && error.details[0].message) {
+			console.error(error.details[0].message);
+			return res.status(400).json({ msg: error.details[0].message });
+		}
+		console.error(error);
+		res.status(500).json({ msg: "Server error" });
+	}
+
+	res.send("Posts route");
+});
+
+export default router;
