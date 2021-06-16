b50afb347b4e6b25b1d931821cec10a0e4c7422b
twilight-warlock <devansh.ts@somaiya.edu>
2021-06-17 01:27:41 +0530

    Added routes to get and delete posts

--- routes/api/posts/posts.js
+++ routes/api/posts/posts.js
@@ -37,7 +37,6 @@ router.post("/", auth, async (req, res) => {
 
 		res.json(post);
 	} catch (error) {
-		console.log(error);
 		if (error && error.details[0].message) {
 			console.error(error.details[0].message);
 			return res.status(400).json({ msg: error.details[0].message });
@@ -45,8 +44,76 @@ router.post("/", auth, async (req, res) => {
 		console.error(error);
 		res.status(500).json({ msg: "Server error" });
 	}
+});
 
-	res.send("Posts route");
+/*
+@route      GET api/posts
+@desc       Get all post
+@access     Private
+*/
+router.get("/", auth, async (req, res) => {
+	try {
+		// Descending order first
+		const posts = await Post.find().sort({ timestamp: -1 });
+		res.json(posts);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ msg: "Server error" });
+	}
+});
+
+/*
+@route      GET api/posts/:post_id
+@desc       Get post by id
+@access     Private
+*/
+router.get("/:post_id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.post_id);
+
+		if (!post) {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+
+		res.json(post);
+	} catch (err) {
+		console.error(err);
+		if (err.kind === "ObjectId") {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+		res.status(500).json({ msg: "Server error" });
+	}
+});
+
+/*
+@route      DELETE api/posts/:post_id
+@desc       de lete a post by id
+@access     Private
+*/
+router.delete("/:post_id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.post_id);
+
+		if (!post) {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+
+		// If user owns the post, only then delete
+		if (post.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: "User not authorized" });
+		}
+
+		//  To remove post
+		await post.remove();
+
+		res.json({ msg: "Post deleted" });
+	} catch (err) {
+		console.error(err);
+		if (err.kind === "ObjectId") {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+		res.status(500).json({ msg: "Server error" });
+	}
 });
 
 export default router;
