f7429cf05311118fbf4cd9557fe56a6bd2bab50b
twilight-warlock <devansh.ts@somaiya.edu>
2021-06-16 12:30:23 +0530

    Added route to delete experience

--- routes/api/profile/experience.js
+++ routes/api/profile/experience.js
@@ -5,15 +5,38 @@ import auth from "../../../middleware/auth.js";
 import joi from "joi";
 
 const router = Router();
-
-const schema =
-	/*
-@route      PUT api/profile
+/*
+@route      PUT api/profile/experience
 @desc       Add experience
 @access     Private
 */
-	router.put("/experience", auth, async (req, res) => {
-		const {
+router.put("/experience", auth, async (req, res) => {
+	const {
+		role,
+		companyName,
+		startDate,
+		employmentType,
+		location,
+		endDate,
+		description,
+		current,
+	} = req.body;
+
+	try {
+		await joi
+			.object({
+				role: joi.string().min(3).max(40).required(),
+				companyName: joi.string().min(3).required(),
+				startDate: joi.date().required(),
+			})
+			.validateAsync({ role, companyName, startDate });
+
+		// Used with .validate and not with validateAsync
+		// if (error) {
+		// 	return res.status(400).json({ msg: error.details[0].message });
+		// }
+
+		const newExp = {
 			role,
 			companyName,
 			startDate,
@@ -22,47 +45,49 @@ const schema =
 			endDate,
 			description,
 			current,
-		} = req.body;
+		};
 
-		try {
-			await joi
-				.object({
-					role: joi.string().min(3).max(40).required(),
-					companyName: joi.string().min(3).required(),
-					startDate: joi.date().required(),
-				})
-				.validateAsync({ role, companyName, startDate });
+		const profile = await Profile.findOne({ user: req.user.id });
+		profile.experience.unshift(newExp);
 
-			// Used with .validate and not with validateAsync
-			// if (error) {
-			// 	return res.status(400).json({ msg: error.details[0].message });
-			// }
+		await profile.save();
 
-			const newExp = {
-				role,
-				companyName,
-				startDate,
-				employmentType,
-				location,
-				endDate,
-				description,
-				current,
-			};
-
-			const profile = await Profile.findOne({ user: req.user.id });
-			profile.experience.unshift(newExp);
-
-			await profile.save();
-
-			res.json(profile);
-		} catch (error) {
-			if (error && error.details[0].message) {
-				console.error(error.details[0].message);
-				return res.status(400).json({ msg: error.details[0].message });
-			}
-			console.error(error);
-			res.status(500).json({ msg: "Server error" });
+		res.json(profile);
+	} catch (error) {
+		if (error && error.details[0].message) {
+			console.error(error.details[0].message);
+			return res.status(400).json({ msg: error.details[0].message });
 		}
-	});
+		console.error(error);
+		res.status(500).json({ msg: "Server error" });
+	}
+});
+
+/*
+@route      DELETE api/profile/experience
+@desc       Add experience
+@access     Private
+*/
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id });
+		const oldLength = profile.experience.length;
+
+		profile.experience = profile.experience.filter(
+			(exp) => exp._id != req.params.exp_id
+		);
+
+		if (profile.experience.length === oldLength) {
+			return res.status(400).json({ msg: "Experience data not found" });
+		}
+
+		await profile.save();
+
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).json({ msg: "Server error" });
+	}
+});
 
 export default router;
