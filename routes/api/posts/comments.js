8d995d795e612d0f8cda123685942dc49b8031ba
twilight-warlock <devansh.ts@somaiya.edu>
2021-06-18 16:24:27 +0530

    Added route to add comments

---
+++ routes/api/posts/comments.js
@@ -0,0 +1,94 @@
+import { Router } from "express";
+import auth from "../../../middleware/auth.js";
+import joi from "joi";
+import User from "../../../models/User.js";
+import Post from "../../../models/Post.js";
+
+const router = Router();
+
+/*
+@route      PUT api/posts/comment/:post_id
+@desc       Comment on a post
+@access     Private
+*/
+router.put("/comment/:post_id", auth, async (req, res) => {
+	const { comment } = req.body;
+
+	try {
+		await joi
+			.object({
+				comment: joi.string().required(),
+			})
+			.validateAsync({ comment });
+
+		const user = await User.findById(req.user.id).select("-password");
+		const post = await Post.findById(req.params.post_id);
+
+		const newComment = {
+			comment,
+			name: user.username,
+			avatar: user.avatar,
+			user: req.user.id,
+		};
+
+		post.comments.unshift(newComment);
+
+		await post.save();
+
+		res.json(post.comments);
+	} catch (error) {
+		if (error && error.details[0].message) {
+			console.error(error.details[0].message);
+			return res.status(400).json({ msg: error.details[0].message });
+		}
+		console.error(error);
+		res.status(500).json({ msg: "Server error" });
+	}
+});
+
+/*
+@route      DELETE api/posts/:post_id
+@desc       delete a comment
+@access     Private
+*/
+router.delete("/comment/:post_id/:comment_id", auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.post_id);
+
+		if (!post) {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+
+		const comment = post.comments.filter(
+			(comment) => comment.id === req.params.comment_id
+		);
+
+		if (!comment) {
+			return res.status(404).json({ msg: "Comment not found" });
+		}
+
+		// If user owns the comment, then delete
+		if (comment[0].user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: "User not authorized" });
+		}
+
+		//  To remove comment
+		const removeIndex = post.comments
+			.map((com) => com.user.toString())
+			.indexOf(req.user.id);
+
+		post.comments.splice(removeIndex, 1);
+
+		await post.save();
+
+		res.json(post.comments);
+	} catch (err) {
+		console.error(err);
+		if (err.kind === "ObjectId") {
+			return res.status(404).json({ msg: "Post not found" });
+		}
+		res.status(500).json({ msg: "Server error" });
+	}
+});
+
+export default router;
--- routes/api/posts/index.js
+++ routes/api/posts/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import posts from "./posts.js";
 import likes from "./likes.js";
+import comments from "./comments.js";
 
 const router = Router();
 
 router.use("/", posts);
 router.use("/", likes);
+router.use("/", comments);
 export default router;
