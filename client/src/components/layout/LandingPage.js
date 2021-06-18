61043d6cc42d0f51dfd81c00608b7409f61c1f24
twilight-warlock <devansh.ts@somaiya.edu>
2021-06-18 23:58:28 +0530

    Home page UI completed

---
+++ client/src/components/layout/LandingPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const LandingPage = () => {
+	return (
+		<section className="landing">
+			<div className="dark-overlay">
+				<div className="landing-inner">
+					<h1 className="supaBig">HighLight</h1>
+					<p className="head">
+						One place for Developers exploring their career paths
+					</p>
+					<div className="button-group">
+						<a href="register.html" className="btn btn-primary2">
+							Sign Up
+						</a>
+						<a href="login.html" className="btn btn-light">
+							Login
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default LandingPage;
