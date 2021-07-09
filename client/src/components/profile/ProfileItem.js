dc755a55c474ca8403972d26db8d4190e39369ac
twilight-warlock <devansh.ts@somaiya.edu>
2021-07-09 16:18:26 +0530

    Minor changes

--- client/src/components/profile/ProfileItem.js
+++ client/src/components/profile/ProfileItem.js
@@ -14,19 +14,23 @@ const ProfileItem = ({
 }) => {
 	return (
 		<div className="profile bg-light">
-			<img src={avatar} alt="avatar" className="round" />
-			<div>
-				<h2>{username}</h2>
-				<h3>{headline}</h3>
-				{summary && <p className="my-2">{summary}</p>}
-				{location && <p className="my-2">{location}</p>}
-				<Link to={`/profile/${_id}`} className="btn btn-primary2">
-					View Profile
-				</Link>
+			<div className="row">
+				<div className="col-12 col-md-4 justify-content-center w-100 align-items-center">
+					<img src={avatar} alt="avatar" className="round" />
+				</div>
+				<div>
+					<h2>{username}</h2>
+					<h3>{headline}</h3>
+					{summary && <p className="my-2">{summary}</p>}
+					{location && <p className="my-2">{location}</p>}
+					<Link to={`/profile/${_id}`} className="btn btn-primary2">
+						View Profile
+					</Link>
+				</div>
 			</div>
 			<ul>
 				{skills.slice(0, 4).map((skill, index) => (
-					<li key={index} className="text-primary">
+					<li key={index} className="badge badge-primary">
 						<i className="fas fa-check"></i> {skill}
 					</li>
 				))}
