a69b0a6258be382ded52fce5b24786de82e30b5f
twilight-warlock <devansh.ts@somaiya.edu>
2021-07-12 19:55:02 +0530

    Added like and unlike functionality

--- client/src/components/post/PostItem.js
+++ client/src/components/post/PostItem.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import formatDate from "../../controller/formatDate";
 import { connect } from "react-redux";
+import { addLike, removeLike } from "../../actions/post";
 
 const PostItem = ({
 	post: {
@@ -17,6 +18,8 @@ const PostItem = ({
 		timestamp,
 	},
 	auth,
+	addLike,
+	removeLike,
 }) => {
 	return (
 		<div class="post bg-white p-1 my-1">
@@ -29,11 +32,19 @@ const PostItem = ({
 			<div>
 				<p class="my-1">{caption}</p>
 				<p class="post-date">Posted on {formatDate(timestamp)}</p>
-				<button type="button" class="btn btn-light">
+				<button
+					type="button"
+					class="btn btn-light"
+					onClick={() => addLike(_id)}
+				>
 					<i class="fas fa-thumbs-up"></i>
 					<span>{likes.length}</span>
 				</button>
-				<button type="button" class="btn btn-light">
+				<button
+					type="button"
+					class="btn btn-light"
+					onClick={() => removeLike(_id)}
+				>
 					<i class="fas fa-thumbs-down"></i>
 				</button>
 				<Link to={`/post/${_id}`} class="btn btn-primary">
@@ -55,10 +66,12 @@ const PostItem = ({
 PostItem.propTypes = {
 	auth: PropTypes.object.isRequired,
 	post: PropTypes.object.isRequired,
+	addLike: PropTypes.func.isRequired,
+	removeLike: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
 	auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
