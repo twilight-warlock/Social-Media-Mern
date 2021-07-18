af33e139c189424ac1e361439a8fb269a426af3a
twilight-warlock <devansh.ts@somaiya.edu>
2021-07-19 02:48:33 +0530

    Production setup added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import userRoutes from "./routes/api/users/users.js";
 import profileRoutes from "./routes/api/profile/index.js";
 import authRoutes from "./routes/api/auth/auth.js";
 import postsRoutes from "./routes/api/posts/index.js";
+import path from "path";
 
 const app = express();
 
@@ -13,13 +14,20 @@ connectDb();
 // Initialize middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Connected"));
-
 // Defining routes
 app.use("/api/users", userRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postsRoutes);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	// public folder
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 // Port and server
 const PORT = process.env.PORT || 5000;
